0b2430fae56504e49c69fadc6aa54e307cfdac00
namitkadam <nk.namitkadam@gmail.com>
2023-11-17 22:18:46 +0530

    import/export

---
+++ ex-im file/script-ex.js
@@ -0,0 +1,8 @@
+function multiply(a, b, c) {
+  return a * b * c;
+}
+
+function addition(a, b, c) {
+  return a + b + c;
+}
+export { multiply, addition };
---
+++ ex-im file/script-im.js
@@ -0,0 +1,7 @@
+// import { multiply, addition } from "./script-ex.js";
+// console.log(multiply(2, 3, 4));
+// console.log(addition(2, 3, 4));
+import * as ex from "./script-ex.js";
+
+console.log(ex.multiply(2, 3, 4));
+console.log(ex.addition(2, 3, 4));
