c6f4ccfb234eb0241ff7708ecf2454d1bbb03e11
namitkadam <nk.namitkadam@gmail.com>
2023-11-28 22:05:01 +0530

    If the user has already logged in, redirect to the home page and vice versa

--- DOM/Authentication/home-page.js
+++ DOM/Authentication/home-page.js
@@ -1,6 +1,14 @@
 const welMsg = document.querySelector("#wel-msg");
 const logOutBtn = document.getElementById("logOut-btn");
-welMsg.innerHTML = `Welcome ${localStorage.userName}`;
+import { user } from "./userData.js";
+
+const findUser = user.find((users) => users.uesrName === localStorage.userName);
+if (!localStorage.userName) {
+  window.location.replace("./index.html");
+} else {
+  welMsg.innerHTML = `Welcome ${findUser.name}`;
+}
+if (!localStorage.userName) window.history.back();
 logOutBtn.addEventListener("click", () => {
   localStorage.clear();
 });
---
+++ DOM/Authentication/userData.js
@@ -0,0 +1,9 @@
+export const user = [
+  { uesrName: "user1", name: "Raj Pal", password: "user12" },
+  { uesrName: "namit", name: "Namit Kadam", password: "12345678" },
+  { uesrName: "James", name: "James Walker", password: "James12" },
+  { uesrName: "Velma", name: "Velma Clemons", password: "Velma12" },
+  { uesrName: "Kibo", name: "Kibo Underwood", password: "Kibo12" },
+  { uesrName: "Louis", name: "Louis Mcgee", password: "Louis12" },
+  { uesrName: "Phyllis", name: "Phyllis Paul", password: "Phyllis12" },
+];
