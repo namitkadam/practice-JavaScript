d5eec4e0ae9e890ead5e982843320c663420e4db
namitkadam <nk.namitkadam@gmail.com>
2023-11-28 22:13:05 +0530

    If the user has already logged in, redirect to the home page and vice versa

--- DOM/Authentication/home-page.js
+++ DOM/Authentication/home-page.js
@@ -1,14 +1,6 @@
 const welMsg = document.querySelector("#wel-msg");
 const logOutBtn = document.getElementById("logOut-btn");
-import { user } from "./userData.js";
-
-const findUser = user.find((users) => users.uesrName === localStorage.userName);
-if (!localStorage.userName) {
-  window.location.replace("./index.html");
-} else {
-  welMsg.innerHTML = `Welcome ${findUser.name}`;
-}
-if (!localStorage.userName) window.history.back();
+welMsg.innerHTML = `Welcome ${localStorage.userName}`;
 logOutBtn.addEventListener("click", () => {
   localStorage.clear();
 });
--- DOM/Authentication/script.js
+++ DOM/Authentication/script.js
@@ -1,44 +1,36 @@
 const userName = document.getElementById("user-name");
 const userPassword = document.getElementById("user-password");
 const loginBtn = document.getElementById("login-btn");
-const checkBtn = document.getElementById("check-btn");
 
-import { user } from "./userData.js";
+let user = [
+  { uesrName: "user1", name: "Raj", password: "user12" },
+  { uesrName: "namit", name: "Namit", password: "12345678" },
+];
 export function loigin() {
   if (loginBtn) {
     loginBtn.addEventListener("click", (e) => {
       const findUser = user.find((user) => user.uesrName === userName.value);
-      e.preventDefault();
+
       if (userName.value === "" || userName.value === null) {
+        e.preventDefault();
         userName.style.border = "2px solid red";
         alert("UserName is required");
       } else if (!findUser) {
+        e.preventDefault();
         userName.style.border = "2px solid red";
         alert("User Not Found");
       } else if (userPassword.value === "" || userPassword.value === null) {
+        e.preventDefault();
         userPassword.style.border = "2px solid red";
         alert("Password is required");
       } else if (findUser.password !== userPassword.value) {
+        e.preventDefault();
         userPassword.style.border = "2px solid red";
         alert("password not mactch");
       } else {
-        window.location.reload();
-        localStorage.setItem("userName", findUser.uesrName);
+        localStorage.setItem("userName", findUser.name);
       }
     });
   }
 }
 loigin();
-
-if (checkBtn) {
-  checkBtn.addEventListener("click", (e) => {
-    if (userPassword.type === "password") {
-      userPassword.type = "text";
-    } else {
-      userPassword.type = "password";
-    }
-  });
-}
-if (localStorage.userName) {
-  window.location.replace("./home-page.html");
-}
